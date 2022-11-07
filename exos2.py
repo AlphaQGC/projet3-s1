a9641b91ddaaef11d5d0593450fac1504a69bb52
Legrix <alegrix@gaming.tech>
2022-11-07 18:52:19 +0100

    exos a faire

---
+++ exos2.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x + y
+
+def sub(x,y): 
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    return x / y
+
+def modulo(x,y):
+
+def SalaireParSeconde(Salairehorraire,heureparjourouvré,Nbjoursouvréparan):
+
+def SalaireNet(Brut,coefficient):
